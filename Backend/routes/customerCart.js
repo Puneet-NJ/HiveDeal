b1212f9959d9dd20760447979b828b44db10cce0
Sadanand Miskin <miskinsadanand@gmail.com>
2024-02-25 22:59:20 +0530

    Update both inc and desc into single route

--- Backend/routes/customerCart.js
+++ Backend/routes/customerCart.js
@@ -1,82 +1,127 @@
-import { Auth } from "../controllers/auth.js";
-import customerCart from "../models/cart.js";
-import customer from "../models/customer.js";
-import express from "express";
-import jwt from "jsonwebtoken";
-import product from "../models/product.js";
+import { Auth } from "../controllers/auth.js" 
+import customerCart from "../models/cart.js" 
+import customer from "../models/customer.js" 
+import express from "express" 
+import jwt from "jsonwebtoken" 
+import product from "../models/product.js" 
 
 // import product from '../models/product'
 
-const router = express.Router();
+const router = express.Router() 
 
 router.get("/cart", Auth, async (req, res) => {
 	try {
 		jwt.verify(req.token, process.env.user_token, async (err, data) => {
 			if (err) {
-				return res.status(403).json("error, forbidden");
+				return res.status(403).json("error, forbidden") 
 			}
-			console.log("data", data.customerEmail);
+			console.log("data", data.customerEmail) 
 			const custCartValue = await customer.findOne({
 				customerEmail: data.customerEmail,
-			});
+			}) 
 
-			// console.log(custID);
+			// console.log(custID) 
 			const cartItems = await customerCart
 				.findOne({ customer: custCartValue._id })
 				.populate("customer")
 				.populate({
 					path:"product.product",
 					model: "Product"
-				});
+				}) 
 
 				console.log(cartItems)
 			res.json({
 				cartItems,
-			});
-		});
+			}) 
+		}) 
 	} catch (err) {
-		console.log(err);
+		console.log(err) 
 	}
-});
+}) 
 
 router.post("/additem", Auth, async (req, res) => {
-    try {
-        const { _id } = req.body;
+    const { _id, isInc } = req.body 
 
-        jwt.verify(req.token, process.env.user_token, async (err, data) => {
-            if (err) {
-                return res.json("error, forbidden");
-            } 
-            
-            const custCartValue = await customer.findOne({
-                customerEmail: data.customerEmail,
-            });
+    //To increase number of items in cart
+    if(isInc){
+        try {
+        
 
-            let cart = await customerCart.findOne({ customer: custCartValue._id });
-            if (!cart) {
-                await customerCart.create({
-                    customer: custCartValue._id,
-                    product: [{ product: _id }]
-                });
-            } else {
-                const productInCart = cart.product.find(p => p.product.toString() === _id);
-				
-                if (productInCart) {
-                    productInCart.totalItems += 1;
+            jwt.verify(req.token, process.env.user_token, async (err, data) => {
+                if (err) {
+                    return res.json("error, forbidden") 
+                } 
+                
+                const custCartValue = await customer.findOne({
+                    customerEmail: data.customerEmail,
+                }) 
+    
+                let cart = await customerCart.findOne({ customer: custCartValue._id }) 
+                if (!cart) {
+                    await customerCart.create({
+                        customer: custCartValue._id,
+                        product: [{ product: _id }]
+                    }) 
                 } else {
-                    cart.product.push({ product: _id });
+                    const productInCart = cart.product.find(p => p.product.toString() === _id) 
+                    
+                    if (productInCart) {
+                        productInCart.totalItems += 1 
+                    } else {
+                        cart.product.push({ product: _id }) 
+                    }
+                    await cart.save()  
                 }
-                await cart.save(); 
-            }
-
-            await product.findByIdAndUpdate(_id, { $inc: { totalItems: 1 } });
-
-            return res.send("Item added to cart");
-        });
-    } catch (err) {
-        return res.json("Internal server error");
+    
+                await product.findByIdAndUpdate(_id, { $inc: { totalItems: 1 } }) 
+    
+                return res.send("Item added to cart") 
+            }) 
+        } catch (err) {
+            return res.json("Internal server error") 
+        }
     }
-});
+
+    //To remove Number of items
+    else{
+        try {
+            const { _id } = req.body 
+    
+            jwt.verify(req.token, process.env.user_token, async (err, data) => {
+                if (err) {
+                    return res.json("error, forbidden") 
+                } 
+                
+                const custCartValue = await customer.findOne({
+                    customerEmail: data.customerEmail,
+                }) 
+    
+                let cart = await customerCart.findOne({ customer: custCartValue._id }) 
+                if (!cart) {
+                    await customerCart.create({
+                        customer: custCartValue._id,
+                        product: [{ product: _id }]
+                    }) 
+                } else {
+                    const productInCart = cart.product.find(p => p.product.toString() === _id) 
+                    
+                    if (productInCart) {
+                        productInCart.totalItems -= 1 
+                    } else {
+                        cart.product.push({ product: _id }) 
+                    }
+                    await cart.save()  
+                }
+    
+                await product.findByIdAndUpdate(_id, { $dec: { totalItems: 1 } }) 
+    
+                return res.send("Item Decreased") 
+            }) 
+        } catch (err) {
+            return res.json("Internal server error") 
+        }
+    }
+}) 
 
 router.post('/removeitem', Auth, async (req, res) => {
   
@@ -93,7 +138,7 @@ router.post('/removeitem', Auth, async (req, res) => {
                 const customerCartValues = await customerCart.findOne(
                     {customer: cust._id}
                 )
-                    console.log(customerCartValues);
+                    console.log(customerCartValues) 
                     const procuctToRemove = customerCartValues.product.filter((item)=> {
                         return item.product.toString() !== _id
                     })
@@ -101,7 +146,7 @@ router.post('/removeitem', Auth, async (req, res) => {
                    customerCartValues.product = procuctToRemove
                    await customerCartValues.save()
                 //    console.log(customerCartValues)
-               // console.log(customerCartValues);
+               // console.log(customerCartValues) 
                    res.json('Item removed Succesfully')
 
                     
@@ -114,45 +159,10 @@ router.post('/removeitem', Auth, async (req, res) => {
 })
 
 
-router.post('/decrement', Auth , (req,res)=>{
-    try {
-        const { _id } = req.body;
-
-        jwt.verify(req.token, process.env.user_token, async (err, data) => {
-            if (err) {
-                return res.json("error, forbidden");
-            } 
-            
-            const custCartValue = await customer.findOne({
-                customerEmail: data.customerEmail,
-            });
-
-            let cart = await customerCart.findOne({ customer: custCartValue._id });
-            if (!cart) {
-                await customerCart.create({
-                    customer: custCartValue._id,
-                    product: [{ product: _id }]
-                });
-            } else {
-                const productInCart = cart.product.find(p => p.product.toString() === _id);
-				
-                if (productInCart) {
-                    productInCart.totalItems -= 1;
-                } else {
-                    cart.product.push({ product: _id });
-                }
-                await cart.save(); 
-            }
-
-            await product.findByIdAndUpdate(_id, { $dec: { totalItems: 1 } });
-
-            return res.send("Item Decreased");
-        });
-    } catch (err) {
-        return res.json("Internal server error");
-    }
-})
+// router.post('/decrement', Auth , (req,res)=>{
+    
+// })
 
 
 
-export default router;
+export default router 
