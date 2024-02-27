f448c10214d5a979d3c424354296593793a69e54
Sadanand Miskin <miskinsadanand@gmail.com>
2024-02-27 23:48:47 +0530

    Create orders based on date for each customer

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 export const db = async () => {
     try {
-       await mongoose.connect(process.env.new_Mongo)
+       await mongoose.connect(process.env.mongo_URI)
         console.log('MongoDB Connected')
     }
     catch(err) {
